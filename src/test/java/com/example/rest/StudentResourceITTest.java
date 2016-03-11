95d31ebd5cd97cd29ae0a05dae626e86b1f53314
ravisankar <ravisankar2gmail.com>
2016-03-11 22:41:45 +1100

    Fixed the Arquillian-Sabot issue by adding the sabot jar in lib folder of the war.

--- src/test/java/com/example/rest/StudentResourceITTest.java
+++ src/test/java/com/example/rest/StudentResourceITTest.java
@@ -2,9 +2,7 @@ package com.example.rest;
 
 import static net.javacrumbs.jsonunit.JsonAssert.assertJsonEquals;
 import static net.javacrumbs.jsonunit.JsonAssert.when;
-import static net.javacrumbs.jsonunit.core.Option.TREATING_NULL_AS_ABSENT;
-import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
-import static net.javacrumbs.jsonunit.core.Option.COMPARING_ONLY_STRUCTURE;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,8 +12,6 @@ import java.nio.file.Paths;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -23,7 +19,7 @@ import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.exporter.ZipExporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -41,7 +37,8 @@ public class StudentResourceITTest {
 				 .addAsWebInfResource("beans.xml")
 				 .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
 				 .addAsResource("test.properties")
-				 .addAsResource("base.properties");
+				 .addAsResource("base.properties")
+				 .addAsLibraries(Maven.resolver().resolve("org.tomitribe:sabot:0.9").withTransitivity().asFile());
 		webArchive.as(ZipExporter.class).exportTo(new File("/home/ravi/hello.war"), true);
 		return webArchive;
 	}
@@ -55,6 +52,14 @@ public class StudentResourceITTest {
 		.request()
 		.post(Entity.entity("{\"name\":\"Hello\"}", MediaType.APPLICATION_JSON), String.class);
 	}*/
+	
+	@Test
+	public void test_sabot() {
+		String response = client.target(baseUrl.toString()+"api/hello")
+								.request()
+								.get(String.class);
+		assertEquals("Hello World !!! Environment : test", response);
+	}
 	
 	@Test
 	public void test_find_student_by_id() {
